48fe087808e3f738377f2240593099814a3dd52d
MUKESH KUMAR <66511917+Mukesh16407@users.noreply.github.com>
2022-08-30 13:03:25 +0530

    completed registration backend

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const User = require('../models/userModel');
-
+const bcrypt = require('bcryptjs');
 
 router.post("/register", async (req, res) => {
   try {
+
     const userExists = await User.findOne({ email: req.body.email });
-  } catch (err) {}
+    if(userExists){
+      return res.status(200).send({ message: "User already exists", success: false });
+    }
+   const password = req.body.password;
+   const salt = await bcrypt.genSaltSync(10);
+   const hashPassword = await bcrypt.hash(password,salt);
+
+   req.body.password = hashPassword;
+
+   const newUser = new User(req.body);
+   await newUser.save();
+
+   res.status(200).send({message:"user created successfully",success:true})
+  } catch (err) {
+    console.log(error);
+    res.status(500).send({ message: "Error creating user", success: false, error });
+  }
 });
 
 router.post("/login", async (req, res) => {
