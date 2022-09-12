b0f8991fb1244ca88c2b4fbafad36fa8b0e64ac8
MUKESH KUMAR <66511917+Mukesh16407@users.noreply.github.com>
2022-09-12 13:00:38 +0530

    add end point

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
 
 const port = process.env.PORT || 5000;
 
-
+app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, async()=>{
     await connect()
     console.log(`Listening on port  ${port}`)
